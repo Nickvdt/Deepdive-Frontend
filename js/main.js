f5b82a5cfc80a47b4d5760421cb7c6a9830e7c94
TijmenT <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>
2023-03-21 01:01:32 -0800

    Fixed JS van Vragenlijst

--- js/main.js
+++ js/main.js
@@ -11,34 +11,49 @@ class Question {
     this.HTMLNextButton = HTMLNextButton;
     this.HTMLPreviousButton.onclick = this.onPreviousClicked;
     this.HTMLNextButton.onclick = this.onNextClicked;
-
     fetch("../vragen.json")
       .then((response) => response.json())
       .then((json) => {
         this.vragen = json;
         this.vraag = newVraag;
         if (this.vraag == null) {
-          this.vraag = 1;
-          localStorage.setItem("currentquestion", 1);
+          this.vraag = 0;
+          localStorage.setItem("currentquestion", 0);
         }
         this.updateVraagText(this.vragen);
       });
   }
-
   updateVraagText(vragen) {
     this.vragen = vragen
     if (this.htmlElement) {
-      this.htmlElement.innerText = this.vragen['vragenlijst'][0]['question']
+      this.htmlElement.innerText = this.vragen['vragenlijst'][parseInt(localStorage.getItem("currentquestion"))]['question']
+      if(localStorage.getItem(parseInt(localStorage.getItem("currentquestion"))) == null){
+        localStorage.setItem(parseInt(localStorage.getItem("currentquestion")), 0)
+        document.getElementById("js--updatedata").value = parseInt(localStorage.getItem("currentquestion"));
+      }
     }
   }
 
     onPreviousClicked = () => {
+      this.htmlElement.innerText = this.vragen['vragenlijst'][parseInt(localStorage.getItem("currentquestion")) - 1]['question']
         localStorage.setItem("currentquestion", parseInt(localStorage.getItem("currentquestion")) - 1)
-        this.htmlElement.innerText = this.vragen['vragenlijst'][parseInt(localStorage.getItem("currentquestion")) - 1]['question']
+        if(localStorage.getItem(parseInt(localStorage.getItem("currentquestion"))) == null){
+          localStorage.setItem(parseInt(localStorage.getItem("currentquestion")), 0)
+          document.getElementById("js--updatedata").value = localStorage.getItem(parseInt(localStorage.getItem("currentquestion")));
+       }else{
+        document.getElementById("js--updatedata").value = localStorage.getItem(parseInt(localStorage.getItem("currentquestion")));
+       }
+        
     }
     onNextClicked = () => {
+      this.htmlElement.innerText = this.vragen['vragenlijst'][parseInt(localStorage.getItem("currentquestion")) + 1]['question']
         localStorage.setItem("currentquestion", parseInt(localStorage.getItem("currentquestion")) + 1)
-        this.htmlElement.innerText = this.vragen['vragenlijst'][parseInt(localStorage.getItem("currentquestion")) + 1]['question']
+        if(localStorage.getItem(parseInt(localStorage.getItem("currentquestion"))) == null){
+          localStorage.setItem(parseInt(localStorage.getItem("currentquestion")), 0)
+          document.getElementById("js--updatedata").value = localStorage.getItem(parseInt(localStorage.getItem("currentquestion")));
+       }else{
+        document.getElementById("js--updatedata").value = localStorage.getItem(parseInt(localStorage.getItem("currentquestion")));
+       }
     }
     
 }
