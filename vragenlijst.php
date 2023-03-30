f06a72cbe9ac208d92c28a3a0cd6af8bec8d2eb1
Nickvdt <32934@ma-web.nl>
2023-03-30 15:52:03 +0200

    vragenlijst responsive gemaakt

--- vragenlijst.php
+++ vragenlijst.php
@@ -32,27 +32,48 @@
           
             <input onChange=UpdateData() id="js--updatedata" type="range" min="1" max="6" step="1" value="2">
         </section>
-
-        <section class="vragenlijst__tabel">
-            <table>
-              <thead>
-                <tr>
-                  <th>Nooit op mijn van toepassing</th>
-                  <th>Is zo nu en dan op mijn van toepassing</th>
-                  <th>Is vaak op mij van toepassing</th>
-                  <th>Is altijd op mij van toepassing</th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr>
-                  <td>1</td>
-                  <td>2/3</td>
-                  <td>4/5</td>
-                  <td>6</td>
-                </tr>
-              </tbody>
-            </table>
-          </section>
+        <section class="vragenlijst__tabel vragenlijst__tabel--desktop">
+          <table>
+            <thead>
+              <tr>
+                <th>Nooit op mijn van toepassing</th>
+                <th>Is zo nu en dan op mijn van toepassing</th>
+                <th>Is vaak op mij van toepassing</th>
+                <th>Is altijd op mij van toepassing</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>1</td>
+                <td>2/3</td>
+                <td>4/5</td>
+                <td>6</td>
+              </tr>
+            </tbody>
+          </table>
+        </section>
+        <section class="vragenlijst__tabel vragenlijst__tabel--responsive">
+          <table>
+            <tbody>
+              <tr>
+                <td >Nooit op mijn van toepassing</td>
+                <td >1</td>
+              </tr>
+              <tr>
+                <td >Is zo nu en dan op mijn van toepassing</td>
+                <td >2/3</td>
+              </tr>
+              <tr>
+                <td >Is vaak op mij van toepassing</td>
+                <td >4/5</td>
+              </tr>
+              <tr>
+                <td >Is altijd op mij van toepassing</td>
+                <td >6</td>
+              </tr>
+            </tbody>
+          </table>
+          </section>     
           <section id="js--nextbuttons" class="vragenlijst__buttons">
             <button id="js--previousbutton" class="vragenlijst__button--previous">< Vorige</button>
             <button id="js--nextbutton" class="vragenlijst__button--next">Volgende ></button>
