15fd91002b75e4c06849ab1665a5fb58746efa17
Nickvdt <32934@ma-web.nl>
2023-04-26 10:55:07 +0200

    Dashboard OOP veder geoptimaliseerd

--- js/dashboardOOP.js
+++ js/dashboardOOP.js
@@ -1,128 +1,155 @@
+class Header {
+  placeToRenderHeader;
+  headerElement;
+  navElement;
+  figureElement;
+  ulElement;
+  ulLiElement;
+  headerUlAElement;
+  headerAElement;
+  imgElement;
+  constructor(placeToRenderHeader) {
+    this.placeToRenderHeader = document.getElementsByTagName(placeToRenderHeader)[0];
+    this.headerElement = document.createElement("header");
+    this.headerElement.classList.add("header");
+
+    this.navElement = document.createElement("nav");
+    this.navElement.classList.add("header__nav");
+
+    this.figureElement = document.createElement("figure");
+    this.figureElement.classList.add("header__figure");
+
+    this.ulElement = document.createElement("ul");
+    this.ulElement.classList.add("header__ul");
+
+    this.ulLiElement = document.createElement("li");
+    this.ulLiElement.classList.add("header__ul--li");
+
+    this.headerUlAElement = document.createElement("a");
+    this.headerUlAElement.classList.add("header__ul--a");
+    this.headerUlAElement.href = "logout.php";
+    this.headerUlAElement.innerText = "Uitloggen";
+
+    this.headerAElement = document.createElement("a");
+    this.headerAElement.classList.add("header__a");
+    this.headerAElement.href = "index.php";
+
+    this.imgElement = document.createElement("img");
+    this.imgElement.classList.add("header__img");
+    this.imgElement.src = "img/logo.webp";
+    this.imgElement.alt = "Logo van doesburg coaching";
+  }
+  render() {
+    this.placeToRenderHeader.appendChild(this.headerElement);
+    this.headerElement.appendChild(this.navElement);
+    this.navElement.appendChild(this.figureElement);
+    this.figureElement.appendChild(this.headerAElement);
+    this.headerAElement.appendChild(this.imgElement);
+    this.navElement.appendChild(this.ulElement);
+    this.ulElement.appendChild(this.ulLiElement);
+    this.ulLiElement.appendChild(this.headerUlAElement);
+  }
+}
+
+
 class Dashboard {
-    constructor(placeToRender) {
-      this.placeToRender = document.getElementsByTagName(placeToRender)[0];
-      this.header = new Header();
-      this.list = new DashboardList();
-    }
-  
-    render() {
-      this.header.render();
-      this.list.render();
-      this.placeToRender.appendChild(this.header.element);
-      this.placeToRender.appendChild(this.list.element);
-    }
-  }
-  
-  class Header {
-    constructor() {
-      this.element = document.createElement("header");
-      this.element.classList = "header";
-  
-      const nav = document.createElement("nav");
-      nav.classList = "header__nav";
-      const figure = document.createElement("figure");
-      figure.classList = "header__figure";
-      const link = document.createElement("a");
-      link.classList = "header__a";
-      link.href = "";
-      const image = document.createElement("img");
-      image.classList = "header__img";
-      image.alt = "Logo van doesburg coaching";
-      image.src = "img/logo.webp";
-      const ul = document.createElement("ul");
-      ul.classList = "header__ul";
-      const li = document.createElement("li");
-      li.classList = "header__ul--li";
-      const ulLink = document.createElement("a");
-      ulLink.classList = "header__ul--a";
-      ulLink.href = "logout.php";
-      ulLink.innerText = "uitloggen"
+  constructor(mainElement) {
+    this.mainElement = mainElement;
 
-  
-      link.appendChild(image);
-      figure.appendChild(link);
-      nav.appendChild(figure);
-      this.element.appendChild(nav);
-      nav.appendChild(ul);
-      ul.appendChild(li);
-      li.appendChild(ulLink);
-     
+    this.ulElement = document.createElement("ul");
+    this.ulElement.classList = "cards";
+    this.items = [
+      {
+        imgSrc: "img/thought_balloon.webp",
+        imgAlt: "Afbeelding van een denk ballon",
+        text: "Gedachten schema",
+        href: "gschema.php",
+        className: "card card__first",
+        aClassName: "card__first--a",
+      },
+      {
+        imgSrc: "img/vertical_traffic_light.webp",
+        imgAlt: "Afbeelding van een stoplicht",
+        text: "Stress stoplicht",
+        href: "stoplicht.php",
+        className: "card card__second",
+        aClassName: "card__second--a",
+      },
+      {
+        imgSrc: "img/writing_hand.webp",
+        imgAlt: "Afbeelding van een checklist icoon",
+        text: "Vragenlijst",
+        href: "vragenlijst.php",
+        className: "card card__third",
+        aClassName: "card__third--a",
+      },
+      {
+        imgSrc: "img/bar_chart.webp",
+        imgAlt: "Afbeelding van een analyse icoon",
+        text: "Vragenlijst Resultaten",
+        href: "results.php",
+        className: " card card__fourth",
+        aClassName: "card__fourth--a",
+      },
+    ];
+  }
 
-    }
-    render() {
-    }
+  render() {
+    this.mainElement.appendChild(this.ulElement);
+    this.items.forEach((item) => {
+      this.liElement = document.createElement("li");
+      this.liElement.classList = item.className;
+
+      this.aElement = document.createElement("a");
+      this.aElement.classList = item.aClassName;
+      this.aElement.href = item.href;
+
+      this.figureElement = document.createElement("figure");
+
+      this.imgElement = document.createElement("img");
+      this.imgElement.src = item.imgSrc;
+      this.imgElement.alt = item.imgAlt;
+      this.imgElement.classList.add('img__card');
+
+      this.spanElement = document.createElement("span");
+      this.spanElement.innerText = item.text;
+
+      this.ulElement.appendChild(this.liElement);
+      this.figureElement.appendChild(this.imgElement);
+      this.aElement.appendChild(this.figureElement);
+      this.aElement.appendChild(this.spanElement);
+      this.liElement.appendChild(this.aElement);
+    });
   }
-  
-  class DashboardList {
-    constructor() {
-      this.element = document.createElement("ul");
-      this.element.classList = "cards";
-  
-      const items = [
-        {
-          imgSrc: "img/thought_balloon.webp",
-          imgAlt: "Afbeelding van een denk ballon",
-          text: "Gedachten schema",
-          href: "gschema.php",
-          className: "card card__first",
-          aClassName: "card__first--a",
-        },
-        {
-          imgSrc: "img/vertical_traffic_light.webp",
-          imgAlt: "Afbeelding van een stoplicht",
-          text: "Stress stoplicht",
-          href: "stoplicht.php",
-          className: "card card__second",
-          aClassName: "card__second--a",
-        },
-        {
-          imgSrc: "img/writing_hand.webp",
-          imgAlt: "Afbeelding van een checklist icoon",
-          text: "Vragenlijst",
-          href: "vragenlijst.php",
-          className: "card card__third",
-          aClassName: "card__third--a",
-        },
-        {
-          imgSrc: "img/bar_chart.webp",
-          imgAlt: "Afbeelding van een analyse icoon",
-          text: "Vragenlijst Resultaten",
-          href: "results.php",
-          className: " card card__fourth",
-          aClassName: "card__fourth--a",
-        },
-      ];
-  
-      items.forEach((item) => {
-        const li = document.createElement("li");
-        li.classList = item.className;
-  
-        const a = document.createElement("a");
-        a.classList = item.aClassName;
-        a.href = item.href;
-  
-        const figure = document.createElement("figure");
-  
-        const img = document.createElement("img");
-        img.src = item.imgSrc;
-        img.alt = item.imgAlt;
-        img.classList.add('img__card');
-  
-        const span = document.createElement("span");
-        span.innerText = item.text;
-  
-        figure.appendChild(img);
-        a.appendChild(figure);
-        a.appendChild(span);
-        li.appendChild(a);
-        this.element.appendChild(li);
-      });
-    }
-    render() {
-      
-    }
+
+}
+
+
+class Main {
+  placeToRenderMain;
+  mainElement;
+  dashboard;
+  constructor(placeToRenderMain) {
+    this.placeToRenderMain = document.getElementsByTagName(placeToRenderMain)[0];
+    this.mainElement = document.createElement("main");
+    this.dashboard = new Dashboard(this.mainElement);
   }
-  
-  const dashboard = new Dashboard("body");
-  dashboard.render();
-  
+  render() {
+    this.placeToRenderMain.appendChild(this.mainElement);
+    this.dashboard.render();
+  }
+}
+
+class App {
+  header;
+  main;
+  constructor() {
+    this.header = new Header("body");
+    this.header.render();
+
+    this.main = new Main("body")
+    this.main.render();
+  };
+}
+
+const app = new App();
